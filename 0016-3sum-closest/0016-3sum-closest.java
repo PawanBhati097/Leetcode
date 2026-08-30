94de139c6d97d62094ee673f9adacf12564af789
PAWAN BHATI <pawanbhati6771@gmail.com>
2026-08-30 23:57:42 +0530

    Time: 17 ms (78.05%), Space: 45.4 MB (66.74%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,27 +1,23 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int closestSum = nums[0] + nums[1] + nums[2];  // Initialize with first triplet
+        int n = nums.length;
+        int closestSum = nums[0] + nums[1] + nums[2];
         
-        for (int i = 0; i < nums.length - 2; i++) {
+        for (int i = 0; i < n - 2; i++) {
             int left = i + 1;
-            int right = nums.length - 1;
+            int right = n - 1;
             
             while (left < right) {
-                int currentSum = nums[i] + nums[left] + nums[right];
+                int sum = nums[i] + nums[left] + nums[right];
                 
-                // If exact match found, return immediately
-                if (currentSum == target) {
-                    return currentSum;
+                if (sum == target) return sum;
+                
+                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                    closestSum = sum;
                 }
                 
-                // Update closest sum if current sum is closer to target
-                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
-                    closestSum = currentSum;
-                }
-                
-                // Move pointers based on comparison with target
-                if (currentSum < target) {
+                if (sum < target) {
                     left++;
                 } else {
                     right--;
